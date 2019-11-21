965db7f539223173998d49b3beffdd25cbce76cc
hirengevariya <hirenb_6165@yahoo.com>
2019-11-21 12:24:51 +0530

    1st ruby programme

---
+++ ruby1.rb
@@ -0,0 +1,41 @@
+my_num = 100    # Add your code here!
+
+my_boolean = true     # And here!
+
+my_string = "Hello Wolrd"    # Also here.
+
+puts my_num
+puts my_boolean
+puts my_string
+
+=begin
+this is 
+a multi line
+comment in ruby
+=end
+
+puts "Hello Ruby!!"
+
+puts "Alok has #{25+30/6} Rupees in his pocket"
+
+cars = 100
+drivers = 30
+puts "There are #{cars} cars and #{drivers} drivers."
+
+print "How old are you? -> "
+age = gets.chomp
+print "How tall are you? -> "
+height = gets.chomp
+puts "You are #{age} year old and your height is #{height} cms"
+
+print "Give a number -> "
+number = gets.chomp.to_i
+puts "You just entered #{number}"
+
+print "Give a number -> "
+number = gets.chomp.to_f
+puts "You just entered #{number}"
+
+print "Give a string -> "
+string = gets.chomp.to_s
+puts "You just entered #{string}"
