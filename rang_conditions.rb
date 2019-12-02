37399605444edbfa770bab5edc8686e9cc8e237a
hirengevariya <hirenb_6165@yahoo.com>
2019-12-02 12:48:30 +0530

    Ruby rang exapmples

---
+++ rang_conditions.rb
@@ -0,0 +1,12 @@
+print "Enter score 0 to 100 value -> "
+score = gets.chomp.to_i
+
+result = case score
+         when 0..40 then "Fail"
+         when 41..60 then "Pass"
+         when 61..70 then "Pass with Merit"
+         when 71..100 then "Pass with Distinction"
+         else "Invalid Score"
+         end
+
+puts result
