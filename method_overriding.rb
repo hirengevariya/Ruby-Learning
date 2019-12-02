3f0f56866996000f7d38030dc849476ca40fa673
hirengevariya <hirenb_6165@yahoo.com>
2019-12-02 18:46:56 +0530

    Method overriding examples

---
+++ method_overriding.rb
@@ -0,0 +1,26 @@
+# define a class
+class Box
+  # constructor method
+  def initialize(w, h)
+    @width, @height = w, h
+  end
+  # instance method
+  def getArea
+    @width * @height
+  end
+end
+
+# define a subclass
+class BigBox < Box
+  # change existing getArea method as follow
+  def getArea
+    @area = @width * @height
+    puts "Bib box area is : #@area"
+  end
+end
+
+# create an object
+box = BigBox.new(10, 20)
+
+# print the area using overriden method
+box.getArea()
