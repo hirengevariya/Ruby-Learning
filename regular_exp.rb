c09daf30cb51744141a832c629b09da068efa662
hirengevariya <hirenb_6165@yahoo.com>
2019-12-04 18:59:43 +0530

    Freezing object example

---
+++ regular_exp.rb
@@ -0,0 +1,31 @@
+line1 = 'Cats are smarter than dogs';
+line2 = 'Dogs also like meat';
+
+if ( line1 =~ /Cats/ )
+  puts 'Line1 contains Cats'
+end
+if ( line2 =~ /Cats(.*)/ )
+  puts 'Line2 contains  Dogs'
+end
+
+print "\n -- Search & Replace with Regular Expration --\n"
+
+phone = "2004-959-559 #This is Phone Number"
+
+# Delete Ruby-style comments
+phone = phone.sub!(/#.*$/, "")
+puts "Phone Num : #{phone}"
+
+# Remove anything other than digits
+phone = phone.gsub!(/\D/, "")
+puts "Phone Num : #{phone}"
+
+print "\n-- 2nd example --\n"
+text = "rails are rails, really good Ruby on Rails"
+
+# Change "rails" to "Rails" throughout
+text.gsub!("rails", "Rails")
+
+# Capitalize the word "Rails" throughout
+text.gsub!(/\brails\b/, "Rails")
+puts "#{text}"
